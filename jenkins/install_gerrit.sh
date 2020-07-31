1234cb541a1ca4f8c19214dc4edc1a2ebc51460e
Vignesh Sweekaran <vignesh.sweekaran@wipro.com>
2020-07-31 17:19:59 +0530

    added the gerrit scripts

--- jenkins/install_gerrit.sh
+++ jenkins/install_gerrit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo yum -y update && sudo yum -y install wget git java-1.8.0-openjdk
-
 wget https://gerrit-releases.storage.googleapis.com/gerrit-3.1.3.war
 export GERRIT_SITE=~/gerrit_testsite
 java -jar gerrit*.war init --batch --dev -d $GERRIT_SITE
--- jenkins/install_jenkins.sh
+++ jenkins/install_jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum -y update && sudo yum -y install wget java-1.8.0-openjdk
+sudo yum -y update && sudo yum -y install git wget java-1.8.0-openjdk
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
